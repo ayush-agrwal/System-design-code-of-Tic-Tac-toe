3ca874ae1988663768d902571ac072b15bffce66
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2024-08-11 12:53:20 +0530

    Create MainClass.java

---
+++ Lowleveldesign/com/lld/tictactoe/MainClass.java
@@ -0,0 +1,12 @@
+package com.lld.tictactoe;
+
+public class MainClass {
+
+	public static void main(String args[]) {
+        TicTacToeGame game = new TicTacToeGame();
+        game.initializeGame();
+        System.out.println("game winner is: " + game.startGame());
+    }
+
+
+}
